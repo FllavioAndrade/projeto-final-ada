e71a310652a9a9301c446464186e14af91ba2baf
FllavioAndrade <flavio.a.s@live.com>
2024-09-04 21:54:26 -0300

    feat: salvando as informações no arqiuvo .txt

--- info_pet.py
+++ info_pet.py
@@ -1,12 +1,33 @@
-
+def salvar_informacoes_do_animal(nome, idade, peso, altura, imc):
+    with open("informacoes_do_animal.txt", "a") as arquivo:
+        arquivo.write(f"Nome: {nome}\n")
+        arquivo.write(f"Idade: {idade} anos\n")
+        arquivo.write(f"Peso: {peso} kg\n")
+        arquivo.write(f"Altura: {altura} cm\n")
+        arquivo.write(f"IMC: {imc:.2f}\n")
+        arquivo.write("-----------------------\n")
+        arquivo.write("\n")
 
 def calcular_imc_do_animal(peso, altura):
-    
+
+    """
+    Calcula o IMC (índice de Massa Corporal) de um animal com base em seu peso e altura.
+
+    :param peso: Peso do animal, em kg
+    :param altura: Altura do animal, em cm
+    :return: IMC do animal
+    """
+
     imc = peso / (altura ** 2)
     return imc
 
 # Função para coletar informações sobre o pet
 def coletar_informacoes_pet():
+    """
+    Coleta informações sobre o pet do usuário, incluindo o nome, idade, peso e altura, e calcula o IMC do pet.
+
+    :return: Nenhum valor é retornado. As informações do pet são exibidas na tela.
+    """
     print("Por favor, insira as informações sobre seu pet.")
 
     # Coleta do nome do pet
@@ -56,6 +77,8 @@ def coletar_informacoes_pet():
     print(f"Peso: {peso} kg")
     print(f"Altura: {altura} cm")
     print(f"IMC do pet: {imc:.2f}")
+
+    salvar_informacoes_do_animal(nome, idade, peso, altura, imc)
 
 # Chama a função para coletar e exibir as informações do pet
 coletar_informacoes_pet()
